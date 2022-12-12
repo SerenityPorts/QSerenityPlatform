5aa62a64999e0bda7ef61d72d57e26b4998d0b68
Clemens Wasser <clemens.wasser@gmail.com>
2022-12-12 17:25:47 +0100

    Migrate QSerenityString to the new AK::String

--- serenity/qserenitystring.h
+++ serenity/qserenitystring.h
@@ -8,15 +8,11 @@
 
 #define AK_DONT_REPLACE_STD
 
-#include <QtCore/private/qstringconverter_p.h>
-#include <forward_list>
-#include <AK/StdLibExtraDetails.h>
-#include <AK/String.h>
+#include <AK/Forward.h>
 #include <QString>
 
-class QSerenityString
+namespace QSerenityString
 {
-public:
-    static AK::String fromQString(const QString& str);
-    static QString toQString(const AK::String& v);
+    AK::ErrorOr<AK::String> fromQString(const QString& str);
+    QString toQString(const AK::String& v);
 };
